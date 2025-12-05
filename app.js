e48c333c86205a728fdfd7801c468876a37d5e1d
kriiscodes <kriiscodes0@gmail.com>
2025-12-05 17:06:44 +0100

    third commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+});
