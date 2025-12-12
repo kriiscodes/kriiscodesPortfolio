d5ffd2b6046c7a814883ba20a4a413ecc2154048
kriiscodes <kriiscodes0@gmail.com>
2025-12-12 23:31:05 +0100

    responsiveness

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 
+
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobileMenu");
 
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("open");
+  document.body.classList.toggle("menu-open");
 });
