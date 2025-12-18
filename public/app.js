3bfb9c3bf2450c5f386a6074c3e9cc3dd7734b41
kriiscodes <kriiscodes0@gmail.com>
2025-12-18 19:08:52 +0100

    modalPage

--- public/app.js
+++ public/app.js
@@ -7,3 +7,20 @@ hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("open");
   document.body.classList.toggle("menu-open");
 });
+
+
+
+const openBtn = document.querySelector('.btnFill');
+const modal = document.getElementById('about');
+const closeBtn = document.getElementById('close');
+
+openBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('show');
+  document.body.classList.add('modal-open');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('show');
+  document.body.classList.remove('modal-open');
+});
